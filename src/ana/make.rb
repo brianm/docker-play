87517024cb22b20ffe50f46d93ddc8bb60cdb795
Brian McCallister <brianm@skife.org>
2013-05-13 17:08:06 +0200

    build ana

--- src/ana/make.rb
+++ src/ana/make.rb
@@ -6,7 +6,7 @@ Docker.make(from: "brianm/ana", to: "brianm/ana") do |b|
   b.maintainer "Brian McCallister <brianm@skife.org>"  
   b.env "DEBIAN_FRONTEND" => "noninteractive",
         "USER" => "xncore",
-        "PORT" => ":8000"
+        "PORT" => "8000"
 
   b.bash <<-EOS
     if [ ! -e /usr/local/bin/node ]
@@ -22,7 +22,7 @@ Docker.make(from: "brianm/ana", to: "brianm/ana") do |b|
   b.bash "rm -rf /app"
   b.put "." => "/app" 
   b.cmd ["/bin/bash", "-c", "cd /app; node app"]
-  b.expose ":8000"
+  b.expose "8000"
 
 end
 
