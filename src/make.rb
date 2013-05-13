b726e6bc6acb2bed3ebf123a8cd223e8ca62ce6f
Brian McCallister <brianm@skife.org>
2013-05-13 17:27:33 +0200

    use port 7000

--- src/make.rb
+++ src/make.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 require "docker/maker"
 
-#Docker.make(from: "ubuntu:12.10", to: "brianm/buildy") do |b|
-Docker.make(from: "brianm/buildy", to: "brianm/buildy") do |b|
+Docker.make(from: "ubuntu:12.10", to: "brianm/buildy") do |b|
+#Docker.make(from: "brianm/buildy", to: "brianm/buildy") do |b|
   b.maintainer "Brian McCallister <brianm@skife.org>"
   b.env "DEBIAN_FRONTEND" => "noninteractive",
         "USER" => "xncore",
-        "PORT" => "8000"
+        "PORT" => "7000"
 
   b.bash <<-EOS
     apt-get install -y bc
@@ -27,7 +27,7 @@ Docker.make(from: "brianm/buildy", to: "brianm/buildy") do |b|
 
   b.put "Procfile" => "/Procfile"
   b.cmd ["/bin/bash", "-c", "honcho start"]
-  b.expose "8000"
+  b.expose "7000"
 
 end
 
