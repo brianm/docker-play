21d408cbcb58e891a85ae41dee0a758e2355e150
Brian McCallister <brianm@skife.org>
2013-05-14 14:45:43 +0200

    prepping for the actual talk

--- src/make.rb
+++ src/make.rb
@@ -1,33 +1,22 @@
 #!/usr/bin/env ruby
 require "docker/maker"
+#Docker.make(from: "ubuntu:12.10", to: "brianm/buildy") do |b|
 
-Docker.make(from: "ubuntu:12.10", to: "brianm/buildy") do |b|
-#Docker.make(from: "brianm/buildy", to: "brianm/buildy") do |b|
+Docker.make(from: "brianm/buildy", to: "brianm/buildy") do |b|
   b.maintainer "Brian McCallister <brianm@skife.org>"
   b.env "DEBIAN_FRONTEND" => "noninteractive",
-        "USER" => "xncore",
-        "PORT" => "7000"
-
+        "USER" => "xncore"
   b.bash <<-EOS
     apt-get install -y bc
     if [ ! -f /var/apt-updated ]; then
       apt-get update
       touch /var/apt-updated
-    else
-      ts=$(echo "$(date +%s) - $(stat -c %Y /var/apt-updated)" | bc)
-      if [ $ts -gt 864000 ]
-      then
-        apt-get update
-        touch /var/apt-updated
-      fi 
     fi
     apt-get install -y netcat python python-pip
     pip install honcho
   EOS
-
   b.put "Procfile" => "/Procfile"
   b.cmd ["/bin/bash", "-c", "honcho start"]
   b.expose "7000"
-
 end
 
